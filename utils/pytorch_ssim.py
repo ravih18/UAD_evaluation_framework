f0f9a4ee96d33b354e7ba74f5e3b50dd64d9ca69
Ravi Hassanaly <42928557+ravih18@users.noreply.github.com>
2023-11-29 16:22:04 +0100

    Added function to have the SSIM map of an image

--- utils/pytorch_ssim.py
+++ utils/pytorch_ssim.py
@@ -195,3 +195,41 @@ def ssim3D(y, y_pred, window_size=11, size_average=True):
     window = window.type_as(img1)
 
     return _ssim_3D(img1, img2, window, window_size, channel, size_average)
+
+
+def ssim_map_3D(y, y_pred, window_size=11, size_average=True):
+    img1 = torch.from_numpy(y)[None, :, :, :, :]  # add a dimension
+    img2 = torch.from_numpy(y_pred)[None, :, :, :, :]
+    (_, channel, _, _, _) = img1.shape
+    window = create_window_3D(window_size, channel)
+
+    if img1.is_cuda:
+        window = window.cuda(img1.get_device())
+    window = window.type_as(img1)
+
+    mu1 = F.conv3d(img1, window, padding=window_size // 2, groups=channel)
+    mu2 = F.conv3d(img2, window, padding=window_size // 2, groups=channel)
+
+    mu1_sq = mu1.pow(2)
+    mu2_sq = mu2.pow(2)
+
+    mu1_mu2 = mu1 * mu2
+
+    sigma1_sq = (
+        F.conv3d(img1 * img1, window, padding=window_size // 2, groups=channel) - mu1_sq
+    )
+    sigma2_sq = (
+        F.conv3d(img2 * img2, window, padding=window_size // 2, groups=channel) - mu2_sq
+    )
+    sigma12 = (
+        F.conv3d(img1 * img2, window, padding=window_size // 2, groups=channel)
+        - mu1_mu2
+    )
+
+    C1 = 0.01**2
+    C2 = 0.03**2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / (
+        (mu1_sq + mu2_sq + C1) * (sigma1_sq + sigma2_sq + C2)
+    )
+    return ssim_map[0][0]
